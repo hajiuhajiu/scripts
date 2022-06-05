a8fd68c9f22f935c844c57b0adfa6d9ef66e5d72
hajiuhajiu <2705322732@qq.com>
2022-06-06 06:15:39 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -29,6 +29,7 @@ ENV_PATH=
 0 19 * * * node /scripts/jd_bean_changes.js >> /log/jd_bean_changes.log 2>&1
 08 01,8 * * * node /scripts/jd_618jk.js >> /log/jd_618jk.log 2>&1
 #01 01  * * * node /scripts/jd_hyj.js >> /log/jd_hyj.log 2>&1
+#01 01,8  * * * node /scripts/jd_cleancart.js >> /log/jd_cleancart.log 2>&1
 13 0-23/2 * * * node /scripts/jd_19e.js >> /log/jd_19e.log 2>&1
 33 0-23/4 * * * node /scripts/jd_bean_home.js >> /log/jd_bean_home.log 2>&1
 0 0 * * * node /scripts/jd_bean_sign.js >> /log/jd_bean_sign.log 2>&1
