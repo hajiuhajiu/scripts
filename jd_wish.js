2c06b9900d9f15b29f2ff630d5bd94e46fb15266
hajiuhajiu <2705322732@qq.com>
2022-06-20 10:40:22 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -72,7 +72,7 @@ if ($.isNode()) {
     if ($.isNode()) await notify.sendNotify($.name, allMessage);
     $.msg($.name, '', allMessage)
   }
-  let res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-Scripts/raw/master/shareCodes/wish.json')
+  let res = await getAuthorShareCode('')
   $.shareCode = [...$.shareCode, ...(res || [])]
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
