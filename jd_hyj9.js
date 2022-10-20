61a67f414a52bfad6ce5849114c2e43792ba92da
hajiuhajiu <2705322732@qq.com>
2022-10-20 18:59:11 +0800

    Update and rename jd_hyj9 to jd_hyj9.js

--- jd_hyj9.js
+++ jd_hyj9.js
@@ -1,10 +1,3 @@
-/*
-
-建议手动先点开一次
-33 0,6-23/3 * * * jd_19E_help.js
-
-*/
-
 const CryptoJS = require("crypto-js");
 const $ = new Env('热爱奇旅互助版-部分加密');
 const notify = $.isNode() ? require('./sendNotify') : '';
