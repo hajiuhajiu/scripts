5590a7664bc3625829a60e72458ccb4689d69144
hajiuhajiu <2705322732@qq.com>
2022-12-09 14:21:23 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -6,14 +6,11 @@
 [task_local]
 #众筹许愿池
 40 0,2 * * * https://raw.githubusercontent.com/KingRan/JDJB/main/jd_wish.js, tag=众筹许愿池, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
 ================Loon==============
 [Script]
 cron "40 0,2 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_wish.js,tag=众筹许愿池
-
 ===============Surge=================
 众筹许愿池 = type=cron,cronexp="40 0,2 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_wish.js
-
 ============小火箭=========
 众筹许愿池 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_wish.js, cronexpr="40 0,2 * * *", timeout=3600, enable=true
  */
@@ -25,8 +22,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ["1FVRZxKiD"];
-let appNameArr = ["超级大转盘"];
+let appIdArr = ["1EFVXyw"];
+let appNameArr = ["年终奖补贴"];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
@@ -141,7 +138,7 @@ async function jd_wish() {
         console.log('抽太多次了，下次再继续吧！');
         break
       }
-      await $.wait(2000)
+      await $.wait(3000)
     }
 
   } catch (e) {
@@ -169,7 +166,7 @@ async function healthyDay_getHomeData(type = true) {
                   if (vo.taskType === 13 || vo.taskType === 12) {
                     console.log(`点击热区`)
                     await harmony_collectScore({ "appId": appId, "taskToken": vo.simpleRecordInfoVo.taskToken, "taskId": vo.taskId, "actionType": "0" }, vo.taskType)
-										await $.wait(1000)
+										await $.wait(1500)
                   } else {
                   console.log(`【${vo.taskName}】已完成\n`)
                 }
@@ -181,14 +178,14 @@ async function healthyDay_getHomeData(type = true) {
                   if (vo.taskType === 13 || vo.taskType === 12) {
                     console.log(`签到`)
                     await harmony_collectScore({ "appId": appId, "taskToken": vo.simpleRecordInfoVo.taskToken, "taskId": vo.taskId, "actionType": "0" }, vo.taskType)
-										await $.wait(1000)
+										await $.wait(1500)
                   } else if (vo.taskType === 1) {
                     for (let key of Object.keys(vo.followShopVo)) {
                       let followShopVo = vo.followShopVo[key]
                       if (followShopVo.status !== 2) {
                         console.log(`【${followShopVo.shopName}】${vo.subTitleName}`)
                         await harmony_collectScore({ "appId": appId, "taskToken": followShopVo.taskToken, "taskId": vo.taskId, "actionType": "0" })
-												await $.wait(1000)
+												await $.wait(1500)
                       }
                     }
                   } else if (vo.taskType === 5) {
@@ -197,7 +194,7 @@ async function healthyDay_getHomeData(type = true) {
                       if (browseShopVo.status !== 2) {
                         console.log(`【${browseShopVo.skuName}】${vo.subTitleName}`)
                         await harmony_collectScore({ "appId": appId, "taskToken": browseShopVo.taskToken, "taskId": vo.taskId, "actionType": "0" })
-												await $.wait(1000)
+												await $.wait(1500)
                       }
                     }
                   } else if (vo.taskType === 15) {
@@ -206,7 +203,7 @@ async function healthyDay_getHomeData(type = true) {
                       if (productInfoVos.status !== 2) {
                         console.log(`【${productInfoVos.skuName}】${vo.subTitleName}`)
                         await harmony_collectScore({ "appId": appId, "taskToken": productInfoVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
-												await $.wait(1000)
+												await $.wait(1500)
                       }
                     }
                   } else if (vo.taskType === 3) {
@@ -215,7 +212,7 @@ async function healthyDay_getHomeData(type = true) {
                       if (shoppingActivityVos.status !== 2) {
                         console.log(`【${vo.subTitleName}】`)
                         await harmony_collectScore({ "appId": appId, "taskToken": shoppingActivityVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
-												await $.wait(1000)
+												await $.wait(1500)
                       }
                     }
                   } else if (vo.taskType === 8) {
@@ -226,7 +223,7 @@ async function healthyDay_getHomeData(type = true) {
                         await harmony_collectScore({ "appId": appId, "taskToken": productInfoVos.taskToken, "taskId": vo.taskId, "actionType": "1" })
                         await $.wait(vo.waitDuration * 1000)
                         await harmony_collectScore({ "appId": appId, "taskToken": productInfoVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
-												await $.wait(1000)
+												await $.wait(1500)
                       }
                     }
                   } else if (vo.taskType === 27 && vo.taskId === 18) {
@@ -242,7 +239,7 @@ async function healthyDay_getHomeData(type = true) {
                           await $.wait(vo.waitDuration * 1000)
                         }
                         await harmony_collectScore({ "appId": appId, "taskToken": shoppingActivityVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
-												await $.wait(1000)
+												await $.wait(1500)
                       }
                     }
                   } else if (vo.taskType === 14) {
